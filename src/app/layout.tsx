a0885c14d4bd49046fca1e6b2352c5b13f9c55a4
Tolunay AKKOYUN <akko_tolu@hotmail.com>
2025-09-03 21:20:59 +0300

    chore: Downgrade to Next.js 14 for stability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import Navbar from "./_components/navbar"; // Navbar'ı import edin
 
 import { TRPCReactProvider } from "~/trpc/react";
@@ -12,19 +13,19 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>
-        <TRPCReactProvider><Navbar />{children}</TRPCReactProvider>
-      </body>
-    </html>
+   <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+  <body>
+    <TRPCReactProvider>
+      <Navbar />
+      {children}
+    </TRPCReactProvider>
+  </body>
+</html>
+
   );
 }
